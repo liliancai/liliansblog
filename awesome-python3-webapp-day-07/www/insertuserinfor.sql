e47b12bc654040d95f5046e6b8b773d6cae5e166
yushun <yushun>
2017-04-08 23:14:30 +0200

    day7

---
+++ awesome-python3-webapp-day-07/www/insertuserinfor.sql
@@ -0,0 +1,4 @@
+USE awesome;
+INSERT INTO users(id,email,passwd,admin,name,image,created_at) VALUES
+('1','admin@test.com','passwd','1','admin','none','0'),
+('2','2@test.com','passwd','0','user2','none','0');
